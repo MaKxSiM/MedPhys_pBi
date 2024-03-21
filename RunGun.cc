5a13aae69b98f98e32fd2d6c41e1b61f3fe1f2c4
Maxim Azarkin <106536388+MaKxSiM@users.noreply.github.com>
2024-03-21 12:48:27 +0300

    Update RunGun.cc

--- RunGun.cc
+++ RunGun.cc
@@ -1,31 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-//
-
-
 #include "DetectorConstruction.hh"
 #include "ActionInitialization.hh"
 #include "MyRunAction.hh"
@@ -71,23 +43,13 @@ int main(int argc,char** argv)
   // Set mandatory initialization classes
   runManager->SetUserInitialization(detector);
 
-  // Physics list
-  //G4VModularPhysicsList* physicsList = new QBBC;
-  //physicsList->SetVerboseLevel(1); // ?
-  //physicsList->RegisterPhysics(new G4StepLimiterPhysics()); //!!!!!!!!!!!!!!!
-  //physicsList->RegisterPhysics(new G4RadioactiveDecayPhysics);
-  //runManager->SetUserInitialization(physicsList);
- // new implimentation of physics physicsList
  PhysicsList* phys = new PhysicsList;
  runManager->SetUserInitialization(phys);
-
-
 
   // User action initialization
   runManager->SetUserInitialization(new MyActionInitialization());
 
   // Initialize visualization
-  //
   G4VisManager* visManager = new G4VisExecutive;
   // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
   // G4VisManager* visManager = new G4VisExecutive("Quiet");
@@ -97,7 +59,6 @@ int main(int argc,char** argv)
   G4UImanager* UImanager = G4UImanager::GetUIpointer();
 
   // Process macro or start UI session
-  //
   if ( ! ui ) {
     // batch mode
     G4String command = "/control/execute ";
